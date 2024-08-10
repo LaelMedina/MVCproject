98abb09aecd2843f10ecfd15cb9cd34ac93a5fb9
LaelMedina <medinalael10@gmail.com>
2024-08-09 18:17:09 -0600

    feat: Modifying the Product class properties

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,14 +1,19 @@
-﻿namespace MVCproyect.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCproyect.Models
 {
     public class Product
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "The Product's name is required")]
         public string Name { get; set; } = string.Empty;
-        
+
+        [Required(ErrorMessage = "The Product's description is required")]
         public string Description { get; set; } = string.Empty;
 
-        public string Price { get; set; } = string.Empty;
+        [Required(ErrorMessage = "The Product's price is required")]
+        public double Price { get; set; }
 
     }
 }
