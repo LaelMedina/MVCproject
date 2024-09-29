b0cfc415fe42c7b394bb6eaad9b1fa9027d5d023
LaelMedina <medinalael10@gmail.com>
2024-09-29 17:55:50 -0600

    feat(model): Adding data annotations

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,19 @@
-﻿namespace MVCproyect.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCproyect.Models
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
 
+        [Required]
         public string UserName { get; set; } = string.Empty;
 
+        [Required]
         public string PasswordHash { get; set; } = string.Empty;
 
+        [Required]
         public int RoleId { get; set; }
 
     }
