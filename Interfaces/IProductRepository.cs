978f8a060f8e5709fc5ee6b3104aa67f1c454d94
LaelMedina <medinalael10@gmail.com>
2024-09-29 18:44:37 -0600

    feat(Interface): Adding the interface layer to the project

---
+++ Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using MVCproyect.Models;
+
+namespace MVCproyect.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<Product> GetProductByIdAsync(int id);
+
+        Task AddProductAsync(Product newProduct);
+        
+        Task UpdateProductAsync(Product newProduct);
+
+        Task DeleteProductAsync(int id);
+    }
+}
