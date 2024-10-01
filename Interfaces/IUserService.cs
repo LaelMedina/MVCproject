491fec9e93b86770bded9cbd7455a30fe08ddee6
LaelMedina <medinalael10@gmail.com>
2024-09-30 19:17:45 -0600

    feat(Interface): Adding the IUserService interface

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using MVCproyect.Models;
+
+namespace MVCproyect.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> GetUserByName(string username);
+
+        Task<bool> ValidateUser(string username, string password);
+    }
+}
--- obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81200a833fcb9cecefd3a6a849ac20f67eabca8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d6969bbe80a80e45ae98a0fcd84407c60e1c64")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
