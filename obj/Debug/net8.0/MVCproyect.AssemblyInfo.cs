b3b097b9011e3e9aab9d480249385d69e35ce572
LaelMedina <medinalael10@gmail.com>
2024-10-01 22:37:18 -0600

    feat(view): Small changes applied to the index form view

--- obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e67794417d942ee13c86c3c2ffb9cd854e8968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f1572311176b68431c25173307aeef43800b6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
