f6a3beea84156e56a5e4d8af957d412afe9a3e0b
LaelMedina <medinalael10@gmail.com>
2024-10-10 21:33:37 -0600

    no info

--- obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4794de45e59e9ae3c8b168539aa63c90ceead15b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d56ff416ecac7fca2e52f659d0b6b1b13da0a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
