4c7f4be3bb748d9c8f4eed8a67f2a9ce0aae4267
LaelMedina <medinalael10@gmail.com>
2024-11-14 05:15:19 -0600

    feat(): Fixing another goddamn bug

--- obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVCproyect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14efe35271e72774c3cc2a9876f845688e70fc87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c6593313a311e73125bd224e7864b97208b7067")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCproyect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
